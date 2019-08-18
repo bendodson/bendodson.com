79cee8a5ae9763ae0eef54f0826b2bc0328a9dc6
Ben Dodson <ben@bendodson.com>
2019-08-18 15:02:57 +0100

    Adding Apple TV artwork finder

---
+++ apple-tv-movies-2019-08-18.js
@@ -0,0 +1,143 @@
+var locale = '';
+var storefront = '';
+
+$(document).ready(function() {  
+    $('#iTunesSearch').submit(function() {
+        performSearch();
+        return false;
+    });
+});
+	
+function performSearch() {
+    $('#results').html('');
+    $('#results').append('<h3>Searching...</h3>');
+
+    var query = $('#query').val();
+    if (!query.length) {
+        return false;
+    };
+
+    locale = window.navigator.userLanguage || window.navigator.language;
+    storefront = $('#storefront').val();
+    
+    $.ajax({
+        type: "POST",
+        crossDomain: true,
+        url: 'https://itunesartwork.bendodson.com/url.php',
+        data: {query: query, storefront: storefront, locale: locale},
+        dataType: 'json'
+    }).done(function(data) {
+
+        $.ajax({
+
+            type: "GET",
+            crossDomain: true,
+            url: data.url,
+            data: {},
+            dataType: 'json'
+
+        }).done(function(data) {
+
+            $.ajax({
+
+                type: "POST",
+                crossDomain: true,
+                url: 'https://itunesartwork.bendodson.com/parse.php',
+                data: { json: JSON.stringify(data), storefront: storefront, locale: locale },
+                dataType: 'json'
+
+            }).done(function(data) {
+
+                $('#results').html('');
+                var html = '';
+
+                if (data.error) {
+                    html = '<h3>'+data.error+'</h3>';
+                    $('#results').append(html);
+                    return;
+                }
+
+                /*
+                if (data.movies.length == 0 && data.shows.length == 1) {
+                    fetchSeasons(data.shows[0].url);
+                    return;
+                }
+                */
+            
+                $('#results').append('<h2>TV Shows</h2>');
+                renderResults(data.shows);
+                addShowLinks();
+                $('#results').append('<h2>Movies</h2>');
+                renderResults(data.movies);
+            });
+            
+        });
+    });
+}
+
+function addShowLinks() {
+    $('#results a.showLink').click(function() {
+        fetchSeasons($(this).attr('rel'));
+        return false;
+    });
+}
+
+function fetchSeasons(url) {
+    $('#results').html('');
+    $('#results').append('<h3>Fetching TV seasons...</h3>');
+    $.ajax({
+
+        type: "GET",
+        crossDomain: true,
+        url: url,
+        data: {},
+        dataType: 'json'
+
+    }).done(function(data) {
+
+        $.ajax({
+
+            type: "POST",
+            crossDomain: true,
+            url: 'https://itunesartwork.bendodson.com/parse.php',
+            data: { json: JSON.stringify(data), storefront: storefront, locale: locale },
+            dataType: 'json'
+
+        }).done(function(data) {
+            $('#results').html('');
+            if (data.error) {
+                $('#results').append('<h3>'+data.error+'</h3>');
+            } else {
+                renderResults(data.seasons);        
+            }
+        });
+        
+    });
+}
+
+function renderResults(results) {
+    if (!results.length) {
+        $('#results').append('<h3>No results found.</h3>');
+    } else {
+        var html = '';
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i];
+            var link = '<a href="'+result.image.url+'" target="_blank" class="imglink">';
+            if (result.url) {
+                link = '<a href="" rel="'+result.url+'" class="showLink imglink">';
+            }
+            html += '<h3>'+result.title+'</h3><div class="tvresult">'+link+'<img src="'+result.image.url+'" alt="Artwork for \''+result.title+'\'" style="background-color: '+result.image.color+'"></a>';
+            html += '<ul>';
+            if (result.url) {
+                html += '<li><strong><a href="" rel="'+result.url+'" class="showLink">View artwork for each season</a></strong></li>';    
+            }            
+            for (var j = 0; j < result.images.length; j++) {
+                var image = result.images[j];
+                html += '<li><a href="'+image.url+'" target="_blank">'+image.name+'</a> ('+image.width+'x'+image.height+'px)</li>';
+            }
+            html += '</ul></div>';
+                            
+        };
+        $('#results').append(html);
+    }
+}
