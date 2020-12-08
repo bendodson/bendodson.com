38f79b14142c0638d82a96fcec76dcce078cc362
Ben Dodson <ben@bendodson.com>
2020-12-08 09:15:28 +0000

    Added query parameters to Apple TV artwork finder

--- apple-tv-movies-2020-12-08.js
+++ apple-tv-movies-2020-12-08.js
@@ -2,11 +2,34 @@ var locale = '';
 var storefront = '';
 
 $(document).ready(function() {  
+
+    var params = getSearchParameters();
+    if (params.query && params.storefront) {
+        $('#query').val(params.query);
+        $('#storefront').val(params.storefront);
+        performSearch();
+    };
+
     $('#iTunesSearch').submit(function() {
         performSearch();
         return false;
     });
 });
+
+function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = decodeURIComponent(tmparr[1]);
+    }
+    return params;
+}
 	
 function performSearch() {
     $('#results').html('');
