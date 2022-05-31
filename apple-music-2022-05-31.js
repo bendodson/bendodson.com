2e142a75d602e17c724dd2f741eafa70ceecb89f
Ben Dodson <ben@bendodson.com>
2022-05-31 11:17:55 +0100

    Update Apple Music Artwork finder to use uncompressed artwork

--- apple-music-2022-05-31.js
+++ apple-music-2022-05-31.js
@@ -23,13 +23,12 @@ function performSearch() {
             html += '<img src="'+data.thumb+'" alt="" width="600" height="600"></a>';
             html += '<ul>';
             html += '<li><a href="'+data.thumb+'" target="_blank">Standard Resolution (1000x1000px)</a></li>';
-            html += '<li><a href="'+data.large+'" target="_blank">Highest Resolution</a> <em><small>This can take a while to load!</small></em></li>';
+            html += '<li><a href="'+data.large+'" target="_blank">Highest Resolution (uncompressed)</a></li>';
             if (data.banner) {
                 html += '<li><a href="'+data.banner+'" target="_blank">Banner Artwork</a></li>';    
             }
             html += '</ul>';
             if (data.type == 'albums') {
-                html += '<p>The album artwork from Apple Music is sometimes lower resolution than that on iTunes, especially for older albums. If this album is available on iTunes, I\'d suggest searching for it with my <a href="/projects/itunes-artwork-finder/">iTunes Artwork Finder</a>.</p>';
                 console.log(data);
                 if (data.tracks.length > 0) {
                     html += '<table id="tracks-table">';
